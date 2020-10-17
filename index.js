df037b27de4e89442a95fc0b968bd364fdfc6492
HughesSeanKyle <khughessean@yahoo.com>
2020-10-17 11:36:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//In node projects, file called index.js by convention
+const express = require('express');
+const app = express();
+
+//vid 10 01:01
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000 //V12 04:45
+//vid 10 06:44
+app.listen(PORT);
---
+++ notes.md
@@ -0,0 +1,8 @@
+package.jSon notes 17/10/2020
+Also steps before deployment vid 12 && 13 @ 0:09
+    Above diagram also in gitHub link
+vid 12 08:40 - discussing engines setup
+Same    09:00 - scripts section node index.js
+        10:30 - gitignore
+
+steps for first time  deployment vid 13 @0:44 
