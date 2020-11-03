2b1d55f6bb9a92dd95740d5a15e48895ef5eee0c
HughesSeanKyle <khughessean@yahoo.com>
2020-11-03 09:34:04 +0200

    V46 - Testing Authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,19 @@
 const passport = require('passport'); //requiring original npm module
 
 
-
 module.exports = (app) => { //v30 - exporting func from this file
     //app = express app object in index.js v30
   app.get(
       '/auth/google',
-      passport.authenticate('google', { //google string part of   GoogleStrategy
+      passport.authenticate('google', { //google string part of GoogleStrategy
           scope: ['profile', 'email'] //What access we need in user profile
       })                                  //Google has more scope options
   );
   
   app.get('/auth/google/callback', passport.authenticate('google'));//google string ID'd by GoogleStrategy.
+
+  app.get('/api/current_user', (req, res) => {
+      res.send(req.user);
+  });
 };
+
