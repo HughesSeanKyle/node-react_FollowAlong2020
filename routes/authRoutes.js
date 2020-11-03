e7b62e020a2a66176d39797defade0a489a47125
HughesSeanKyle <khughessean@yahoo.com>
2020-11-03 09:46:47 +0200

    V47 - Logging Out Users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,8 +12,18 @@ module.exports = (app) => { //v30 - exporting func from this file
   
   app.get('/auth/google/callback', passport.authenticate('google'));//google string ID'd by GoogleStrategy.
 
+  app.get('/api/logout', (req, res) => {
+      req.logout();//attached by passport
+      res.send(req.user);//prove to user that they are logged out
+      //req.user will give blank screen - indicates user destroyed. 
+  });
+
   app.get('/api/current_user', (req, res) => {
       res.send(req.user);
   });
 };
+
+/*
+1. req.logout() is a function that is attached automatically to the request object by passport 
+*/
 
