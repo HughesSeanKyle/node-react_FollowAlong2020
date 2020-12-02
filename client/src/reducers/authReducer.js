bb86d61daaa595c03495b74d585b4bcbc4bc9c6d
HughesSeanKyle <khughessean@yahoo.com>
2020-12-02 14:47:55 +0200

    V72 - The Auth Reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+export default function (state = {}, action) {
+    switch (action.type) {
+        default:
+            return state; 
+    }
+}
+
+/*
+First Arg - No initial state hence state equal to empty object. 
+Second Arg - Responsible for changing state. 
+*/
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+// The index.js file in any direcory can be thought of as
+    // an index for a book where each book represents a 
+        // new folder/directory
+        // All important import statements for directory will be here
+            // same like the index.js representing the server.  
