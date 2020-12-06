5c94724ecc74674a5e4df5c7dc3f87366ca125f5
HughesSeanKyle <khughessean@yahoo.com>
2020-12-06 12:25:01 +0200

    V80 - Webpack with CSS

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'materialize-css/dist/css/materialize.min.css'; // 3 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -40,5 +41,11 @@ note //2
 The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function. Since any React component in a React Redux app can be connected, most applications will render a <Provider> at the top level, with the entire app's component tree inside of it.
 
 The provider tag is a react component that knows how to read changes from the redux store anytime the redux store get some new state produced inside of it the provider will inform all of it's child components that some new state is available and will update all those different components with the new state. 
+
+note //3
+When importing css it is important to add the css extension. When the extension is not added webpack will assume that it is javascript.
+
+No need to provide relative path ('./') as webpack will automatically assume you are trying to import an npm module.
+    When CSS is imported like this we do not need to assign it to a variable as it will not be used in this way. 
 
 */
