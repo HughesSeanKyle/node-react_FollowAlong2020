5a6407f0b35909bfa3fcbc5b120edeb05df8da7e
HughesSeanKyle <khughessean@yahoo.com>
2020-12-09 15:02:52 +0200

    V86 - Testing FetchUser

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'; 
 import { BrowserRouter, Route } from 'react-router-dom'; //1
+import { connect } from 'react-redux';  // 3
+import * as actions from '../actions'; // 4
 
 import Header from './Header';
  
@@ -10,9 +12,9 @@ const Landing = () => <h2>Landing</h2>
 
 class App extends Component { //2
     componentDidMount() {
+        this.props.fetchUser();
+    };
 
-    }
-    
     render() {
         return (
             <div className="container"> 
@@ -29,7 +31,7 @@ class App extends Component { //2
     };
 };
 
-export default App; 
+export default connect(null, actions)(App); //5 
 
 /*
 //1
@@ -45,4 +47,14 @@ In Route component the "/" refers to the ROOT route. e.g emaily.com or in dev ju
 ("exact={true}" === "exact") = Make sure that the path is exactly root || localhost:3000 if landing to be displayed. (Do NOT Greedily match.)
 
 The <Header /> will always be visible as it is not tied to any specific configuration. 
+
+// 3
+Call the connect function to give certian components the abiltiy to call action creators. 
+
+// 4
+Imported all different actions from actions file (* Take all different actions that have been defined and just assign them to the actions object)
+
+//5 - Rembember IIFE explanation for this (In advanced JS section of Web dev course)
+    1st arg: Map state to props, will not use on App component therefore === null
+    2nd Arg: All different action creators to wire up - When all actions passed in they are assigned to the App component as props(Which means these can be passed down to any child component). 
 */
