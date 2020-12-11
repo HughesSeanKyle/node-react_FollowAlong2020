241105787d74a638ea72318547f4f510e565bac0
HughesSeanKyle <khughessean@yahoo.com>
2020-12-11 13:19:00 +0200

    V89 - Accessing State in the Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still deciding';
+            case false:
+                return 'I am Logged out';
+            default:
+        }       return 'I am Logged in';
+    }
+
     render() {
         return (
             <nav>
@@ -9,9 +20,7 @@ class Header extends React.Component {
                         Emaily
                     </a>
                     <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -19,4 +28,25 @@ class Header extends React.Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) { // 1
+    return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
+
+/*
+This comes from fetch user ajax request to server. 
+
+// not refactored would look this way
+
+function mapStateToProps(state) { // 1
+    return { auth: state.auth };
+};
+
+console.log will output below
+    1st: indicates still making request - do not have user yet
+    2nd: indicates got user and isLoggedIn.
+
+{auth: null, dispatch: ƒ}
+{auth: {…}, dispatch: ƒ} => If isNotLoggedIn auth === false.
+*/
