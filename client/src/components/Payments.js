5ab9f5d1c189e297ec42d0c9adb283c4f109bc5c
HughesSeanKyle <khughessean@yahoo.com>
2020-12-21 14:07:44 +0200

    V101 - The payments Component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+    render() {
+        debugger;
+        
+        return ( // 1
+            <StripeCheckout 
+                amount={500}
+                token={token => console.log(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            />
+        );
+    }
+}
+
+export default Payments;
+
+/*
+//1 - amount={500} = $5
+//2 - Token = User enters details, details to stripe, stripe sends back token (kindof like Oauth procedure) - *Note - This is a callback - It will trigger a redirect on Token Receipt. 
+*/
