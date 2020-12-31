3ed88667dd50a7b66cfd2907de57eb5b7a267e32
HughesSeanKyle <khughessean@yahoo.com>
2020-12-31 11:13:52 +0200

    V112 - Requiring Authentication

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,12 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
+        // Naive approach to checking if user logged in
+        // The check below only applies to this.route
+        if (!req.user) {
+            // Set 401 error(Unauthorized or forbidden)
+            return res.status(401).send({ error: 'You must log in' })
+        }
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
@@ -22,6 +28,8 @@ module.exports = app => {
 //1
 passport makes it possible to access the user with the 'user' keyword. 
 It is set up automatically by passport with the passport.initialize and passport.session calls in index.js. (NB! this does not save the new info)
+
+the 'user' keyword will be undefined if client is not logged in. 
 
 // 2
 this maintains an up to date copy of user model
